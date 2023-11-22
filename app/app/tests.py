29af0b5c886af82e0887e7c293c450f4ce658686
Your Name <you@example.com>
2023-11-22 21:05:19 +0100

    Fixed python sytnax

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,14 +1,15 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
-    
+
     def test_add_numbers(self):
-        resolutin = calc.add(5,6)
+        resolutin = calc.add(5, 6)
 
-        self.assertEqual(resolutin,11)
-    
+        self.assertEqual(resolutin, 11)
+
     def test_subtract_numbers(self):
-        resolutin = calc.subtract(10,6)
+        resolutin = calc.subtract(10, 6)
 
-        self.assertEqual(resolutin,4)
+        self.assertEqual(resolutin, 4)
